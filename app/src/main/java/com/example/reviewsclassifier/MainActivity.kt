747dc61a8a6b92c7330e866c497ab6deee158601
usesnames <emanueledri@libero.it>
2021-03-31 17:23:58 +0200

    added pie chart and solved configuration change crash

--- app/src/main/java/com/example/reviewsclassifier/MainActivity.kt
+++ app/src/main/java/com/example/reviewsclassifier/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.reviewsclassifier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.chaquo.python.Python
-import com.chaquo.python.android.AndroidPlatform
+import com.anychart.AnyChartView
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,20 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        var anyChartView = findViewById<AnyChartView>(R.id.pie_chart)
 
-        Python.start(AndroidPlatform(this))
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         viewModel.percentuale.observe(this, Observer{
             findViewById<TextView>(R.id.main_text).text = it
         })
-
         viewModel.lastReview.observe(this, Observer{
             findViewById<TextView>(R.id.last_review).text = it
         })
-
         viewModel.buttonVisibility.observe(this, Observer{
             findViewById<Button>(R.id.button).visibility = it
+        })
+        viewModel.pieReady.observe(this, Observer{
+            if(it==true) {
+                anyChartView.setChart(viewModel.pie)
+                anyChartView.visibility = View.VISIBLE
+            }
         })
 
         findViewById<Button>(R.id.button).setOnClickListener {
@@ -38,4 +42,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 }
--- app/src/main/java/com/example/reviewsclassifier/MainViewModel.kt
+++ app/src/main/java/com/example/reviewsclassifier/MainViewModel.kt
@@ -6,12 +6,16 @@ import android.view.View
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.anychart.AnyChart
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.charts.Pie
 import com.chaquo.python.Kwarg
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.tensorflow.lite.task.text.nlclassifier.BertNLClassifier
 
@@ -25,19 +29,27 @@ class MainViewModel(application: Application) : AndroidViewModel(application){
     var percentuale: MutableLiveData<String> = MutableLiveData("Premi il bottone")
     var lastReview: MutableLiveData<String> = MutableLiveData()
     var buttonVisibility: MutableLiveData<Int> = MutableLiveData(View.VISIBLE)
+    var pieReady: MutableLiveData<Boolean> = MutableLiveData(false)
+    var pie = AnyChart.pie()
 
     fun getInsight(){
         buttonVisibility.value = View.INVISIBLE
         viewModelScope.async(Dispatchers.IO) {
+            Python.start(AndroidPlatform(context))
             val py = Python.getInstance()
             val scraper = py.getModule("google_play_scraper")
-            val result = scraper.callAttr("reviews", "com.latuabancaperandroid", Kwarg("count", 100), Kwarg("country", "it"), Kwarg("lang", "it"))
+            val result = scraper.callAttr(
+                "reviews", "com.latuabancaperandroid", Kwarg("count", 100), Kwarg(
+                    "country",
+                    "it"
+                ), Kwarg("lang", "it")
+            )
             reviews = result.asList()[0].asList().map { el -> el.asMap()[PyObject.fromJava("content")].toString() }
             startModel()
         }
     }
 
-    suspend fun startModel() {
+    private suspend fun startModel() {
         return withContext(Dispatchers.IO) {
             val classifier = BertNLClassifier.createFromFile(context, "quantized_mb.tflite")
             var positive = 0.0
@@ -46,17 +58,25 @@ class MainViewModel(application: Application) : AndroidViewModel(application){
                 counter += 1
                 if(counter==1){
                     if (classifier.classify(element)[0].score < 0.5)
-                        lastReview.postValue("Ultima review: "+reviews[0]+"\n\nclassificazione: positiva")
+                        lastReview.postValue("Ultima review: " + reviews[0] + "\n\nclassificazione: positiva")
                     else
-                        lastReview.postValue("Ultima review: "+reviews[0]+"\n\nclassificazione: negativa")
+                        lastReview.postValue("Ultima review: " + reviews[0] + "\n\nclassificazione: negativa")
                 }
-                percentuale.postValue( "avanzamento: $counter / 100")
+                percentuale.postValue("avanzamento: $counter / 100")
                 Log.e("", percentuale.value!!)
 
                 if (classifier.classify(element)[0].score < 0.5) {
                     positive += 1
                 }
             }
+
+            val data: MutableList<DataEntry> = ArrayList()
+            data.add(ValueDataEntry("Positive", positive))
+            data.add(ValueDataEntry("Negative", 100-positive))
+            pie.background().fill("#C9D2D3");
+            pie.data(data)
+            pie.legend().enabled(true)
+            pieReady.postValue(true)
             percentuale.postValue("$positive% di recensioni positive")
             buttonVisibility.postValue(View.VISIBLE)
         }
