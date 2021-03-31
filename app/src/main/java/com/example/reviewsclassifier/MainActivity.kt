dce5c71b2516e66c186a5efe6f6c37c0027cb0fa
usesnames <emanueledri@libero.it>
2021-03-31 14:53:22 +0200

    Improved layout and added ScrollView

--- app/src/main/java/com/example/reviewsclassifier/MainActivity.kt
+++ app/src/main/java/com/example/reviewsclassifier/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.reviewsclassifier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
